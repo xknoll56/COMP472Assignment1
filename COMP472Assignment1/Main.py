02c41c4fa91065253112140f7edfdc415a6bf9a0
Jordan Goulet <jordang1995@hotmail.com>
2021-05-28 03:21:29 -0400

    Role P first heuristic implementation

--- COMP472Assignment1/Main.py
+++ COMP472Assignment1/Main.py
@@ -38,14 +38,14 @@ def main():
     write_to_file("test.txt", map)
     map = load_from_file("test.txt")
     #map = Map()
-    v = RoleV(map)
+    v = RoleP(map)
     bot_left: Zone = map.zones[0][map.columns-1]
     top_right: Zone = map.zones[map.rows-1][0]
 
     start = time.time()
-    v.generate_path(bot_left, top_right)
+    v.generate_path(map.zones[0][0], map.zones[0][1])  # TODO end points are chosen by the user.
     print("Time taken: "+str(time.time()-start))
-    print(v.path[len(v.path)-1].g_value)
+    # print(v.path[len(v.path)-1].g_value)
     print(map)
     zone: Zone
     end_locations: list[Zone] = list()
@@ -58,6 +58,12 @@ def main():
     for loc in end_locations:
         print(str(loc.x)+","+str(loc.y))
     draw_map(map, v)
+
+# Test main function
+def main():
+    map = load_from_file("test.txt")
+    v = RoleP(map)
+    print(v.get_heuristic(map.zones[1][1].upper_left_node, map.zones[2][3].down_right_node))
 
 main()
 
--- COMP472Assignment1/RoleP.py
+++ COMP472Assignment1/RoleP.py
@@ -12,6 +12,65 @@ class RoleP(Role):
 
 
     def generate_path(self, start_zone: Zone, end_zone: Zone):
-        pass
+        # TODO This code is placeholder
+        self.path.append(start_zone.upper_left_node)
+        self.path.append(end_zone.upper_left_node)
+        self.path[1].prevNode = self.path[0]
+
+    # finds the cost of the Best-First Search solution path directly towards target.
+    def best_first_search(self, orig: Node, targ: Node):
+        print("---")
+        # the cost of the lowest cost direct transition towards target.
+        cost = float("inf")
+        # the node with the lowest cost of the lowest cost direct transition towards target.
+        best = None
+        # distance to goal horizontally
+        dx = targ.x - orig.x
+        # distance to goal vertically
+        dy = targ.y - orig.y
+        print("dx=" + str(dx) + ", dy=" + str(dy))
+        if dx > 0:  # the target is to the right of the current node
+            print("target is to the right")
+            edge_cost = self.get_cost_cardinal(orig.right_edge)
+            print("the edge's cost is: " + str(edge_cost))
+            if edge_cost < cost:  # this transition is more cost effective than current best.
+                cost = edge_cost
+                best = orig.right_node
+
+        if dx < 0:  # the target is to the left of the current node
+            print("target is to the left")
+            edge_cost = self.get_cost_cardinal(orig.left_edge)
+            print("the edge's cost is: " + str(edge_cost))
+            if edge_cost < cost:  # this transition is more cost effective than current best.
+                cost = edge_cost
+                best = orig.left_node
+
+        if dy > 0:  # the target is to the bottom of the current node
+            print("target is to the bottom")
+            edge_cost = self.get_cost_cardinal(orig.down_edge)
+            print("the edge's cost is: " + str(edge_cost))
+            if edge_cost < cost:  # this transition is more cost effective than current best.
+                cost = edge_cost
+                best = orig.lower_node
+
+        if dy < 0:  # the target is to the top of the current node
+            print("target is to the top")
+            edge_cost = self.get_cost_cardinal(orig.up_edge)
+            print("the edge's cost is: " + str(edge_cost))
+            if edge_cost < cost:  # this transition is more cost effective than current best.
+                cost = edge_cost
+                best = orig.upper_node
+
+        if cost == float("inf"):
+            return cost
+        if best is None:
+            return 0  # origin is target
+        else:
+            return cost + self.best_first_search(best, targ)
+
+    def get_heuristic(self, start: Node, targ: Node):
+        # the total cost of traversing the Best-First Search solution path directly towards target.
+        print("Initiating heuristic function for Role P with nodes: (" + str(start.x) + ", " + str(start.y) + "), (" + str(targ.x) + ", " + str(targ.y) + ")")
+        return self.best_first_search(start, targ)
 
 
